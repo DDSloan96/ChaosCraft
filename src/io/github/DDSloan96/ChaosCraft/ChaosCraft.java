5bb0d93da5bf0de6af5edde6c589e975768ceb69
DDSloan96 <DDSloan96@aol.com>
2013-12-09 22:49:05 -0500

    Initial Commit, Runnable Plugin Base

---
+++ src/io/github/DDSloan96/ChaosCraft/ChaosCraft.java
@@ -0,0 +1,13 @@
+package io.github.DDSloan96.ChaosCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChaosCraft extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		getLogger().info("Hello World");
+	}
+	@Override
+	public void onDisable(){
+	}
+}
